b94a8e0e203b9e3185121d44ca84fc5fde311302
François Schmidts <francois@schmidts.fr>
2018-10-31 18:54:42 +0100

    removing pipenv as a needed dependencies

--- setup.py
+++ setup.py
@@ -1,23 +1,15 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-REQUIREMENTS = []
-
-try:
-    from pipenv.project import Project
-    from pipenv.utils import convert_deps_to_pip
-    pfile = Project(chdir=False).parsed_pipfile
-    requirements = convert_deps_to_pip(pfile['packages'], r=False)
-except ImportError:
-    requirements = ['pymongo==2.8', 'python-slugify==1.2.6']
+REQUIREMENTS = ['pymongo==2.8', 'python-slugify==1.2.6']
 
 
 setup(name='ad-geo-backend',
-      version='1.1.0',
+      version='1.1.1',
       description='Geographical data management',
       url='https://github.com/dolead/ad-geo-backend',
       author_email='backend@dolead.com',
-      install_requires=requirements,
+      install_requires=REQUIREMENTS,
       packages=find_packages(),
       entry_points={
           'console_scripts': [
