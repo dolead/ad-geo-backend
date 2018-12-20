f1d9d7e6a345e9dce760cab28b4668c13fec6faa
Fabien Bourrel <Far3t@users.noreply.github.com>
2018-12-20 09:55:58 +0100

    Translate + fix hierarchy (#1)

--- ad_geo_backend/shell.py
+++ ad_geo_backend/shell.py
@@ -6,8 +6,8 @@ logger = logging.getLogger(__name__)
 
 
 def load():
-    google_file, bing_file, french_pc_file, iso_code_file \
-            = cmd_line_utils.parse_args()
+    google_file, bing_file, french_pc_file, spain_pc_file, iso_code_file, \
+        alt_names, cities = cmd_line_utils.parse_args()
     google_backend = MongoBackend('GOOGLE')
     bing_backend = MongoBackend('BING') if bing_file else None
 
@@ -18,14 +18,25 @@ def load():
     iso_code_mapping = None
     if iso_code_file:
         iso_code_mapping = common.parse_iso_codes(iso_code_file)
-    google_trans = google.Translator(slug_to_gps, iso_code_mapping)
-    bing_trans = bing.Translator(slug_to_gps, iso_code_mapping) \
-            if bing_file else None
+
+    alt_names_mapping = None
+    cities_mapping = None
+    if alt_names and cities:
+        alt_names_mapping = common.parse_alt_names(alt_names)
+        cities_mapping = common.parse_cities(cities)
+
+    google_trans = google.Translator(slug_to_gps, iso_code_mapping,
+                                     alt_names_mapping, cities_mapping)
+    bing_trans = bing.Translator(slug_to_gps, iso_code_mapping,
+                                 alt_names_mapping, cities_mapping) \
+        if bing_file else None
 
     if google_file:
         google_backend.reset()
         google_backend.check_indexes()
         common.load_file_to_backend(google_backend, google_file, google_trans)
+        google.correct_fr_hierarchy(google_backend, french_pc_file)
+        google.correct_es_hierarchy(google_backend, spain_pc_file)
     if bing_file:
         bing_backend = MongoBackend('BING')
         bing_backend.reset()
